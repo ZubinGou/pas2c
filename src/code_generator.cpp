7dd30521fea4a70af49a55a637fe3698b4b7da19
Terence <i386681404@qq.com>
2021-04-20 20:23:15 +0800

    FEA: update code_generator

--- src/code_generator.cpp
+++ src/code_generator.cpp
@@ -4,8 +4,329 @@
 using namespace std;
 
 string CodeGenerator::run() {
-  programstruct();
+  programstruct(1);
   add_indent();
   return this->target_code;
 }
 
+/*
+  target_code manipulate
+*/
+
+void target_append(std::string code); // 部分代码到目标代码
+void add_indent(); // 为目标代码添加缩进
+
+/* 
+  Parser interface
+*/
+
+std::vector<int> CodeGenerator::get_son(int node_id){} // return tree[node_id].son
+
+int CodeGenerator::get_father(int node_id){} // return tree[node_id].father
+
+void CodeGenerator::match(int node_id, std::string token){} // match current node and token
+
+std::string CodeGenerator::get_token(int node_id){
+  return tree[node_id].type;
+}
+
+std::string CodeGenerator::get_str_value(int node_id) {
+  return tree[node_id].str_value;
+}
+
+double CodeGenerator::get_num_value(int node_id) {
+  return tree[node_id].num_value;
+}
+
+
+/* 
+  Semantic intrface
+*/
+
+bool CodeGenerator::is_addr(std::string id) {
+  return table_ctrl.is_addr(id, state_stack.top());
+}
+
+bool CodeGenerator::is_func(std::string id) {
+  return table_ctrl.is_func(id);
+}
+
+std::string CodeGenerator::get_var_type(std::string id) {
+  return table_ctrl.get_type(id, state_stack.top());
+}
+
+std::string CodeGenerator::get_exp_type(int node_id) {
+  return table_anal.get_exp_type(node_id, state_stack.top());
+}
+
+std::vector<int> CodeGenerator::get_bound(std::string id) {
+  std::vector<Argument> arguments_lists = table_ctrl.get_range(id, state_stack.top());
+  std::vector<int> bound;
+  for (auto argument : arguments_lists) {
+    bound.push_back(argument.period_element.first);
+  }
+  return bound;
+}
+
+std::vector<bool> CodeGenerator::get_args(std::string id) {
+  return table_ctrl.get_args(id);
+}
+
+/*
+  Production
+*/
+
+// programstruct -> program_head ; program_body .
+void CodeGenerator::programstruct(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+
+  if (son_num == 4) {
+    target_append("#include<stdio.h>\n");  // TODO:是否更改头文件解析方式？
+    program_head(son[0]);
+    program_body(son[2]);
+    state_stack.pop();
+  }
+}
+
+// program_head -> program id ( idlist ) | program id
+void CodeGenerator::program_head(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+
+  if (son_num == 5) {
+    // id_list(son[3]);    # 程序头的id_list参数在C语言中不需要
+  }
+  else if (son_num == 2) {
+    
+  }
+}
+
+// program_body -> const_declarations var_declarations subprogram_declarations compound_statement
+void CodeGenerator::program_body(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+
+  if (son_num == 4) {
+    const_declarations(son[0]);
+    var_declarations(son[1]);
+    subprogram_declarations(son[2]);
+    compound_statement(son[3]);
+  }
+}
+
+// idlist -> idlist , id | id
+void CodeGenerator::idlist(int node_id, vector<int> id_num, string id_type="", bool id_addr=false) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+
+  if (son_num == 3) {
+    idlist(son[0], id_num, id_type, id_addr);
+    
+    string value = get_str_value(son[2]);
+    if (id_addr)
+      value = '*' + value;
+    if (id_type == "")  // 声明在变量区
+      target_append(", " + value);
+    else  // 在参数区
+      target_append(", " + id_type + " " + value);
+    if (!id_num.empty())
+      for (auto num : id_num)
+        target_append("[" + to_string(num) + "]");
+  }
+  else if (son_num == 1) {
+    string value = get_str_value(son[0]);
+    if (id_addr)
+      value = '*' + value;
+    target_append(" " + value);
+    if (!id_num.empty())
+      for (auto num : id_num)
+        target_append("[" + to_string(num) + "]");
+  }
+}
+
+// const_declarations -> const const_declaration ; | e
+void CodeGenerator::const_declarations(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 3) {
+    const_declaration(son[1]);
+    target_append(";\n");
+  }
+}
+
+// const_declaration -> const_declaration ; id = const_value | id = const_value 
+void CodeGenerator::const_declaration(int node_id) {
+
+}
+
+// const_value -> + num | - num | num | 'letter'
+void CodeGenerator::const_value(int node_id) {
+
+}
+
+// var_declarations -> var var_declaration ; | e
+void CodeGenerator::var_declarations(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 3) {
+    var_declaration(son[1]);
+    target_append(";\n");
+  }
+}
+
+// var_declaration -> var_declaration ; idlist : type | idlist : type
+void CodeGenerator::var_declaration(int node_id) {
+
+}
+
+// type -> basic_type | array [ period ] of basic_type
+void CodeGenerator::type(int node_id) {
+
+}
+
+// basic_type -> integer | real | boolean | char
+std::string CodeGenerator::basic_type(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 1) {
+    map<string, string> type_pas2c;
+    type_pas2c["integer"] = "int";
+    type_pas2c["real"] = "float";
+    type_pas2c["boolean"] = "bool";
+    type_pas2c["char"] = "char";
+    
+    string var_type = get_str_value(son[0]);
+    if (type_pas2c.find(var_type) != type_pas2c.end())
+      return type_pas2c[var_type];
+    else{
+      ;
+    }
+  }
+}
+
+// period -> period , digits .. digits | digits .. digits
+int CodeGenerator::period(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 3) {
+    int left = get_num_value(son[0]);
+    int right = get_num_value(son[2]);
+    return (right-left+1);  // 1..10 代表10个元素
+  }
+  else if (son_num == 5) {
+    int nums = period(son[0]);
+    int left = get_num_value(son[2]);
+    int right = get_num_value(son[4]);
+    return (nums+right-left+1);
+  }
+}
+
+// subprogram_declarations -> subprogram_declarations subprogram ; | e
+void CodeGenerator::subprogram_declarations(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 3) {
+    subprogram_declarations(son[0]);
+    subprogram(son[1]);
+  }
+}
+
+// subprogram -> subprogram_head ; subprogram_body
+void CodeGenerator::subprogram(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 3) {
+    subprogram_head(son[0]);
+    subprogram_body(son[2]);
+    state_stack.pop();
+  }
+}
+
+// subprogram_head -> procedure id formal_parameter | function id formal_parameter : basic_type 
+void CodeGenerator::subprogram_head(int node_id) {  // 函数头 Pascal-S的过程为没有返回值的函数
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 3) {  // procedure
+    target_append("void");  // void
+    string value = get_str_value(son[1]);
+    state_stack.push(value);
+    target_append(" " + value);  // 函数名 gcd
+
+    formal_parameter(son[2]);
+  }
+  else if (son_num == 5) {  // function
+    string id_type = basic_type(son[4]);  // int float bool char
+    target_append(id_type);  // int
+    string id_value = get_str_value(son[1]);
+    state_stack.push(id_value);
+    target_append(" " + id_value);  // 函数名 gcd
+
+    formal_parameter(son[2]);  // 参数
+  }
+}
+
+// formal_parameter -> ( parameter_list ) | e
+void CodeGenerator::formal_parameter(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 3) {
+    target_append("(");
+    parameter_list(son[1]);
+    target_append(")");
+  }
+  else if (son_num == 1) {
+    target_append("()");
+  }
+}
+
+// parameter_list -> parameter_list ; parameter | parameter
+void CodeGenerator::parameter_list(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 3) {
+    parameter_list(son[0]);
+    target_append(", ");  // 逗号分隔符
+    parameter(son[2]);
+  }
+  else if (son_num == 1) {
+    parameter(son[0]);
+  }
+}
+
+// parameter -> var_parameter | value_parameter
+void CodeGenerator::parameter(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 1) {
+    string token = get_token(son[0]);
+    if (token == "var_parameter")  // 传地址
+        var_parameter(son[0]);
+    else  // 传值
+        value_parameter(son[0]);
+  }
+}
+// var_parameter -> var value_parameter
+void CodeGenerator::var_parameter(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 2) {
+    value_parameter(son[1]);
+  }
+}
+// value_parameter -> idlist : basic_type
+void CodeGenerator::value_parameter(int node_id) {
+  vector<int> son = get_son(node_id);
+  int son_num = son.size();
+  if (son_num == 3) {
+    string id_type = basic_type(son[2]);  // 基本类型 如 int
+    target_append(id_type);  // int
+
+    int parent = get_father(node_id);
+    string token = get_token(parent);
+    if (token == "var_parameter")
+        idlist(son[0], , id_type, true);  // 参数 id,id,id
+    else
+        idlist(son[0], , id_type);
+  }
+}
--- src/code_generator.h
+++ src/code_generator.h
@@ -8,20 +8,6 @@
 
 /*
 struct Node {
- public:
-  Node(int father, int id, bool is_terminal, std::string type = "",
-           double num_value = 0, std::string str_value = "", int line = 0,
-           int col = 0)
-      : father(father),
-        son_num(0),
-        id(id),
-        type(type),
-        num_value(num_value),
-        str_value(str_value),
-        is_terminal(is_terminal),
-        line(line),
-        col(col){};
-
   int father;             // 父节点的编号
   int son_num;            // 子结点的个数
   int id;                 // 节点编号
@@ -32,45 +18,108 @@ struct Node {
   int line;                // 如果是终结符，终结符的行号
   int col;                // 如果是终结符，终结符的列号
   std::vector<int> son;   // 子节点的编号集
-
 };
 */
 
 class CodeGenerator {
  public:
-  CodeGenerator(std::vector<Node> syntax_tree, SemanticAnalyzer semantic_analyzer) {
+  CodeGenerator(const std::vector<Node>& syntax_tree, const SemanticAnalyzer& semantic_analyzer) {
     this->tree = syntax_tree;
-    this->table = semantic_analyzer.controller;
+    this->table_anal = semantic_analyzer;
+    this->table_ctrl = semantic_analyzer.controller;
   }
   
   std::string run(); // 返回 target_code
 
  private:
   std::vector<Node> tree; // 语法树的根节点
-  SymbolTableController table; // 符号表控制器
+  SemanticAnalyzer table_anal;
+  SymbolTableController table_ctrl; // 符号表控制器
   std::stack<std::string> state_stack; // 状态栈，标记作用域
   std::string target_code; // 目标代码
 
   // target_code manipulate
-  void append(std::string code); // 部分代码到目标代码
+  void target_append(std::string code); // 部分代码到目标代码
   void add_indent(); // 为目标代码添加缩进
 
   // Parser interface
-  vector<int> get_child(int node_id); // return tree[node_id].son
+  std::vector<int> get_son(int node_id); // return tree[node_id].son
   int get_father(int node_id); // return tree[node_id].father
-  void match(int node_id, string token); // match current node and token
+  void match(int node_id, std::string token); // match current node and token
+  std::string get_token(int node_id);
+  std::string get_str_value(int node_id);
+  double get_num_value(int node_id);
 
   // Semantic intrface
-  bool id_addr(string id);
-  bool is_func(string id);
-  string get_var_type(string id);
-  string get_exp_type(int node_id);
-  vector<int> get_bound(string id);
-  vector<bool> get_args(string id);
+  bool is_addr(std::string id);
+  bool is_func(std::string id);
+  std::string get_var_type(std::string id);
+  std::string get_exp_type(int node_id);
+  std::vector<int> get_bound(std::string id);
+  std::vector<bool> get_args(std::string id);
 
   // Production
+  /*
+  programstruct -> program_head ; program_body .
+  program_head -> program id ( idlist ) | program id
+  program_body -> const_declarations var_declarations subprogram_declarations compound_statement
+  idlist -> idlist , id | id
+  const_declarations -> const const_declaration ; | e
+  const_declaration -> const_declaration ; id = const_value | id = const_value 
+  const_value -> + num | - num | num | 'letter'
+  var_declarations -> var var_declaration ; | e
+  var_declaration -> var_declaration ; idlist : type | idlist : type
+  type -> basic_type | array [ period ] of basic_type
+  basic_type -> integer | real | boolean | char
+  period -> period , digits .. digits | digits .. digits
+  subprogram_declarations -> subprogram_declarations subprogram ; | e
+  subprogram -> subprogram_head ; subprogram_body
+  subprogram_head -> procedure id formal_parameter | function id formal_parameter : basic_type 
+  formal_parameter -> ( parameter_list ) | e
+  parameter_list -> parameter_list ; parameter | parameter
+  parameter -> var_parameter | value_parameter
+  var_parameter -> var value_parameter
+  value_parameter -> idlist : basic_type
+  */
+  void programstruct(int node_id);
+  void program_head(int node_id);
+  void program_body(int node_id);
+  void idlist(int node_id, vector<int> id_num, string id_type="", bool id_addr=false);
+  void const_declarations(int node_id);
+  void const_declaration(int node_id);
+  void const_value(int node_id);
+  void var_declarations(int node_id);
+  void var_declaration(int node_id);
+  void type(int node_id);
+  std::string basic_type(int node_id);
+  int period(int node_id);
+  void subprogram_declarations(int node_id);
+  void subprogram(int node_id);
+  void subprogram_head(int node_id);
+  void formal_parameter(int node_id);
+  void parameter_list(int node_id);
+  void parameter(int node_id);
+  void var_parameter(int node_id);
+  void value_parameter(int node_id);
 
-
+  /*
+  subprogram_body -> const_declarations var_declarations compound_statement
+  compound_statement -> begin statement_list end
+  statement_list -> statement_list ; statement | statement
+  statement -> variable assignop expression | procedure_call | compound_statement | if expression then statement else_part | for id assignop expression to expression do statement | read ( variable_list ) | write ( expression_list ) | e
+  variable_list -> variable_list , variable | variable
+  variable -> id id_varpart
+  id_varpart -> [ expression_list ] | e
+  procedure_call -> id | id ( expression_list )
+  else_part -> else statement | e
+  expression_list -> expression_list , expression | expression
+  expression -> simple_expression relop simple_expression | simple_expression
+  simple_expression -> simple_expression addop term | term
+  term -> term mulop factor | factor
+  factor -> num | variable | id ( expression_list ) | ( expression ) | not factor | uminus factor
+  */
+  void subprogram_body(int node_id);
+  void compound_statement(int node_id);
 
   // debug
   void print_target_code(); // 打印目标代码
