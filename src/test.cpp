f1a9620c56ee5c0d455076786f9c28a14a4c4679
ztqakita <ztqakita@163.com>
2021-05-06 20:18:33 +0800

    fix one warning

--- src/test.cpp
+++ src/test.cpp
@@ -1,25 +1,25 @@
-#include "parser.h"
-#include "code_generator.h"
-#include "semantic_analyzer.h"
+// #include "parser.h"
+// #include "code_generator.h"
+// #include "semantic_analyzer.h"
 
 
-using namespace std;  // recommend only use it in .cpp
+// using namespace std;  // recommend only use it in .cpp
 
-const string GRAMMAR_FILE = "../include/grammar.json";
-const string INPUT_FILE = "../example/gcd.pas";
+// const string GRAMMAR_FILE = "../include/grammar.json";
+// const string INPUT_FILE = "../example/gcd.pas";
 
-int main() {
-  cout << "\n*** Section 0 ***\n";
-  Parser parser(GRAMMAR_FILE, INPUT_FILE);
-  SyntaxTree tree = parser.generate_tree();
+// int main() {
+//   cout << "\n*** Section 0 ***\n";
+//   Parser parser(GRAMMAR_FILE, INPUT_FILE);
+//   SyntaxTree tree = parser.generate_tree();
 
-  cout << "\n*** Section 1 ***\n";
-  SemanticAnalyzer semantic_analyzer(tree);
-  semantic_analyzer.print_table();
+//   cout << "\n*** Section 1 ***\n";
+//   SemanticAnalyzer semantic_analyzer(tree);
+//   semantic_analyzer.print_table();
 
-  cout << "\n*** Section 2 ***\n";
-  CodeGenerator code_generator(tree.node_dic, semantic_analyzer);
-  cout << code_generator.run() << endl;
+//   cout << "\n*** Section 2 ***\n";
+//   CodeGenerator code_generator(tree.node_dic, semantic_analyzer);
+//   cout << code_generator.run() << endl;
 
-  return 0;
-}
+//   return 0;
+// }
