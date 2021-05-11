d12c1ed3f11730058a7dd1bf4ce4724009368b00
ZubinGou <zebgou@gmail.com>
2021-05-11 09:49:32 +0800

    upload lastest

--- src/main.cpp
+++ src/main.cpp
@@ -6,20 +6,20 @@
 using namespace std;  // recommend only use it in .cpp
 
 const string GRAMMAR_FILE = "../include/grammar.json";
-const string INPUT_FILE = "../example/quicksort.pas";
+const string INPUT_FILE = "../example/gcd.pas";
 
 int main() {
   cout << "\n*** Section 0 ***\n";
   Parser parser(GRAMMAR_FILE, INPUT_FILE);
-  // SyntaxTree tree = parser.generate_tree();
+  SyntaxTree tree = parser.generate_tree();
 
-  // cout << "\n*** Section 1 ***\n";
-  // SemanticAnalyzer semantic_analyzer(tree);
-  // semantic_analyzer.print_table();
+  cout << "\n*** Section 1 ***\n";
+  SemanticAnalyzer semantic_analyzer(tree);
+  semantic_analyzer.print_table();
 
-  // cout << "\n*** Section 2 ***\n";
-  // CodeGenerator code_generator(tree.node_dic, semantic_analyzer);
-  // cout << code_generator.run() << endl;
+  cout << "\n*** Section 2 ***\n";
+  CodeGenerator code_generator(tree.node_dic, semantic_analyzer);
+  cout << code_generator.run() << endl;
 
   return 0;
 }
